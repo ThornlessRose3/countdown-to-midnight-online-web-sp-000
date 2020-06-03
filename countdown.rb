8f2adf5d95c817af0502336dbace7e13d7fc984b
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-03 00:19:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,13 @@
 #write your code here
-
-
-
+def countdown_with_sleep(int)
+  x = int
+  while x < 0
+    puts "#{x} SECOND(S)!"
+    sleep x
+    x -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
 
 def countdown(number)
   x = number
